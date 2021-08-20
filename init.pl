cc95e24bf82134374f7ea9c518e123e0535293b8
マサチ <zhangjb42@gmail.com>
2021-08-20 23:22:47 +0800

    Update init.pl

--- init.pl
+++ init.pl
@@ -14,4 +14,14 @@
     registration_ids=>["jnVAPODuZ4TsgpzNy2ftNMs700l0uemnu8DYsrzFnM1HAa9/vVbA+7B/K84gb2iD"],
     is_ban_official=>1
  });
+ $client->load("PostQRcode",data=>{
+    smtp    =>  'smtp.yandex.com', #邮箱的smtp地址
+    port    =>  '465', #smtp服务器端口，默认25
+    from    =>  'rubbish.masachi@yandex.com', #发件人
+    to      =>  '1248076945@qq.com', #收件人 867303288
+    user    =>  'rubbish.masachi@yandex.com', #smtp登录帐号
+    pass    =>  'qkkjsqyttrnvsprr', #smtp登录密码
+    tls     =>  1,      #可选，是否使用SMTPS协议，默认为0                            
+                        #在没有设置的情况下，如果使用的端口为465，则该选项会自动被设置为1
+});
  $client->run();
